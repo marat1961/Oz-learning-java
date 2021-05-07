25433576e0c5c35ca80be9c3af1407f2515165ec
marat1961 <marat.sh.1961@gmail.com>
2021-05-07 23:57:19 +0700

    initial

---
+++ Test/src/Oz/Main.java
@@ -0,0 +1,11 @@
+package Oz;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        double d = Double.POSITIVE_INFINITY - Double.POSITIVE_INFINITY;
+//        double d = -Double.POSITIVE_INFINITY;
+        double d = 34 / 0.0;
+        System.out.println(d);
+    }
+}
